a6b7eb7f8d3e0d7077d45f734fe8e08f5f2ea6a0
RedMatOnyx <art17004@byui.edu>
2021-02-08 20:56:03 -0800

    presentation imgDemo

---
+++ Presentation/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('aa').innerHTML = "1. object-fit: none";
+document.getElementById('bb').innerHTML = "2. object-fit: fill (default)";
+document.getElementById('cc').innerHTML = "3. object-fit: contain";
+document.getElementById('dd').innerHTML = "4. object-fit: scale-down";
+document.getElementById('ee').innerHTML = "5. object-fit: cover";

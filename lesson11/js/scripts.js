f9c026da3f4bba6eb449f8af51bf8e19bd617fa5
RedMatOnyx <art17004@byui.edu>
2021-03-18 22:45:56 -0700

    the weather site rough together

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -1,5 +1,4 @@
-// lesson 10 current version "skyline" 
-// ::::::::::CURRENT DATE ::::::::::::::::
+/*-------------------- CURRENT DATE ----------------------*/
 //const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 //document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
@@ -40,7 +39,7 @@ document.getElementById('currentDate').textContent = currentdate;
 
 document.getElementById("copyrightYear").innerHTML = todaysdate.getFullYear();
 
-// ::::::::::HAMBURGER MENU ::::::::::::::::
+/*-------------------- HAMBURGER MENU ----------------------*/
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation');
 
@@ -49,7 +48,7 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-// :::::::::::PANCAKE NOTICE ON FRIDAYS ::::::::::::::::::::
+/*-------------------- PANCAKE NOTICE ON FRIDAYS ----------------------*/
 let day = new Date().getDay();
 if (day == 5) {
     // document.querySelector(".message").style.backgroundColor = "pink";
@@ -60,31 +59,43 @@ document.querySelector(".message").style.display = "block";
 // To change the display property in JavaScript, consider the use of .style.display = "block" in a selection structure 
 // where the condition looks at the day of the week through the Date() object and getDay() method.
 
-// ::::::::::::::WEB FONT LOADER ::::::::::::::::::::
+/*-------------------- WEB FONT LOADER ----------------------*/
 WebFont.load({google: {families: ['Kufam']}});
-          
 
-// :::::::::::::::WIND CHILL CALCULATION ::::::::::::::::
-// temporarily removed for this week, calculation done below in another section
-
-
-// ::::::::::::CONTACT SYMBOLS :::::::::::::::::::::::::
+/*--------------------CONTACT SYMBOLS ----------------------*/
 src='https://kit.fontawesome.com/a076d05399.js'
 
-// :::::::::::::PROGRESSIVE LAZY LOAD GALLERY IMAGES :::::::::::::::::
-// found in lazy.js
+/*-------------------- PROGRESSIVE LAZY LOAD GALLERY IMAGES -----------------*/
+// done seperately in lazy.js
 
-/*---------------------STORM CENTER SEVERITY RANGE INPUT---------------*/
+/*--------------------- STORM CENTER SEVERITY RANGE INPUT ---------------*/
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
 
-/*-----------------------API live weather data----------------------*/
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b";
+/*----------------------- API live weather data ----------------------*/
+
+const whichtown = document.querySelector('#wayfindertitle').textContent;
+if (whichtown.includes("Preston")) {
+    locationid = 'id=5604473';
+}
+else if (whichtown.includes("Soda")) {
+    locationid = 'id=5607916';
+}
+else {
+    locationid = 'lat=42.0380399&lon=-111.4048681';
+}
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?${locationid}&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b`;
+
+// 5604473 for preston
+// 5607916 for Soda Springs
+// lat=42.0380399&lon=-111.4048681 for Fish Haven
+
 fetch(apiURL)
   .then((response) => response.json())
-  .then((jsObject) => {
-    //   console.log(jsObject);
+  .then((jsObject) => { 
+      //   console.log(jsObject);
 
     const currdesc = document.querySelector('#currdesc');
     const humidity = document.querySelector('#humidity');
@@ -98,22 +109,18 @@ fetch(apiURL)
     windy.innerHTML = Math.round(jsObject.wind.speed);
     humidity.innerHTML = jsObject.main.humidity;
 
-// wind chill
-// pulled from lesson 7
-// let t = document.getElementById("currTemp").innerHTML;
-// let s = document.getElementById("speed").innerHTML;
+// wind chill code pulled from lesson 7
 if(jsObject.main.temp < 50 && jsObject.wind.speed > 3) {
     let windchill = Math.round(35.74 + 0.6215 * jsObject.main.temp - 35.75 * jsObject.wind.speed **0.16 + 0.4275 * jsObject.main.temp * jsObject.wind.speed**0.16);
     document.getElementById("chill").innerHTML = `${windchill} &#8457;`;
 } else {document.getElementById("chill").innerHTML = "N/A"}
   });
 
-  /*----------------------5 DAY FORCAST API------------------------*/
-//   const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b";
-const apiURL2 = `https://api.openweathermap.org/data/2.5/forecast?${locationid}&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b`;
+  /*---------------------- 5 DAY FORCAST API ------------------------*/
+    const apiURL2 = `https://api.openweathermap.org/data/2.5/forecast?${locationid}&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b`;
 fetch(apiURL2)
   .then(response => response.json())
-  .then(jsObject => {
+  .then(jsObject => { 
     // console.log(jsObject);
 
     //string.includes(18:00:00) Method ... or string.indexof()
@@ -123,12 +130,12 @@ fetch(apiURL2)
     const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     forecast.forEach(x => {
         const d = new Date(x.dt_txt);
-        
+
         document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
         document.getElementById(`forecast${day+1}`).textContent = Math.round(x.main.temp);
-
+    
         const weathericon = document.querySelector(`icon${day+1}`);
-    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+            // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
     //rather do string interpolation below instead
     const isrc = `https://openweathermap.org/img/w/${forecast[day].weather[0].icon}.png`;
     const desc = forecast[day].weather[0].description; //delete
